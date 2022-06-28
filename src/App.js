ae95417e7a2ff29be4372e5a961a1221efc44ad2
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-06-29 03:44:07 +0500

    Checking correct answer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
 function App() {
-  const { waiting, loading, questions, index, correct, nextQuestion } =
-    useGlobalContext();
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext();
 
   if (waiting) {
     return <SetupForm />;
@@ -35,6 +42,7 @@ function App() {
                   className="answer-btn"
                   key={index}
                   dangerouslySetInnerHTML={{ __html: answer }}
+                  onClick={() => checkAnswer(correct_answer === answer)}
                 ></button>
               );
             })}
